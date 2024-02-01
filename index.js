6007790dbecc02eaf5b631be3aad2e4f7634ba51
Ankush  Baburao Yamkar <yamkar.ab10@gmail.com>
2024-02-01 08:00:06 +0530

    removed likes and comments

--- index.js
+++ index.js
@@ -327,7 +327,7 @@ app.post("/post-video-facebook", async (req, res) => {
 
 app.get("/get-facebook-posts", async (req, res) => {
   const response = await axios.get(
-    `https://graph.facebook.com/v19.0/214846575048732/feed?access_token=${process.env.meta_access_token}&fields=message,full_picture,likes.limit(1).summary(true),comments.limit(1).summary(true),shares`
+    `https://graph.facebook.com/v19.0/214846575048732/feed?access_token=${process.env.meta_access_token}&fields=message,full_picture`
   );
   res.send(response.data);
 });
